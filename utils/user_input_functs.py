9f541f350d83bacd415ad85ae66c61b195aff4b2
Denys <herasymuk@ucu.edu.ua>
2020-08-25 00:54:34 +0300

    - add 0_data_preprocessing.ipynb

---
+++ utils/user_input_functs.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+def init_tool_config_arg():
+    parser = argparse.ArgumentParser(description="Step #3.Prepare dialogs data.")
+    parser.add_argument(
+        "--dialogs_ids",
+        nargs="+",
+        type=int,
+        help="id(s) of dialog(s) to download, -1 for all",
+        required=False,
+    )
+    parser.add_argument(
+        "--config_path",
+        type=str,
+        help="path to config file",
+        default="config/config.json",
+    )
+    parser.add_argument("--debug_mode", type=int, help="Debug mode", default=0)
+    return parser.parse_args()
